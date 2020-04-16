849198a4d5468a516b01bd132f789bfa2c6a898d
JSpringwater92 <jeffreyspringwater@gmail.com>
2020-04-16 16:21:48 -0400

    //Added events.js file to Books scripts --Created events.js file to hold events --Created events function for when button is clicked

--- assets/scripts/books/events.js
+++ assets/scripts/books/events.js
@@ -1,3 +1,12 @@
 'use strict'
 
 const api = require('./api.js')
+
+
+const onRetrievAllBooks = function(event) {
+    event.preventDefault()
+
+    api.retrieveAllBooks()
+      .then(retrieveAllBooksSuccess)
+      .catch(retrieveAllBooksFail)
+}
